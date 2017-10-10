ed3226fe97902a41ed6c2df2fe7f658c40a8ba5b
Kem Crimmins <kem.crimmins@gmail.com>
2017-10-10 18:05:39 -0400

    add getNaturalTime, getUnixTime

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ var app = express();
 
 app.use('/:timestamp', (req, res) => {
 	var timestamp = req.params.timestamp;
-	var naturalTime = new Date(timestamp);
-	res.send(naturalTime);
-	// console.log(timestamp);
-	// res.send(timestamp);
+	var naturalTime; 
+	var unixTime;
+
+	console.log(typeof timestamp);
+	if (timestamp.indexOf(' ') === -1) {
+		unixTime = Number.parseInt(timestamp, 10);
+		naturalTime = getNaturalTime(unixTime * 1000);
+	} else {
+		naturalTime = timestamp;
+		unixTime = getUnixTime(timestamp);
+	}
+
+	res.send(naturalTime + ', ' + unixTime); 
 });
 
 app.use('/', (req, res) => {
@@ -17,3 +26,19 @@ app.use('/', (req, res) => {
 app.listen(3000, ()=> {
 	console.log('Listening on port 3000');
 });
+
+function getNaturalTime(unixTime) {
+	var date = new Date(unixTime);
+	var options = {year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC'};
+
+	return date.toLocaleString('en-US', options);
+}
+
+function getUnixTime(naturalTime) {
+	var date = new Date(naturalTime);
+	var year = date.getFullYear();
+	var month = date.getMonth();
+	var day = date.getDate();
+
+	return Date.UTC(year, month, day)/1000;
+}
